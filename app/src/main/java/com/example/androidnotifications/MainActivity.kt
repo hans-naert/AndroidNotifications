6599691d2eaef996de9cfb2add3c58df1e863258
Hans Naert <hans.naert@vives.be>
2023-03-20 22:04:51 +0100

    -show NotificationActivity when clicking on notification -request permission for showing notifications

--- app/src/main/java/com/example/androidnotifications/MainActivity.kt
+++ app/src/main/java/com/example/androidnotifications/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.androidnotifications
 
+import android.Manifest.permission.ACCESS_NOTIFICATION_POLICY
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.example.androidnotifications.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,18 +30,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= 32) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    ACCESS_NOTIFICATION_POLICY
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+                return;
+            val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission(),
+                { Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show() })
+            launcher.launch(POST_NOTIFICATIONS);
+        }
+
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val channel = NotificationChannel("normal", "Normal",NotificationManager.IMPORTANCE_DEFAULT)
+        val channel =
+            NotificationChannel("normal", "Normal", NotificationManager.IMPORTANCE_DEFAULT)
         manager.createNotificationChannel(channel)
 
 
         binding.sendNoticeButton.setOnClickListener {
+            val intent = Intent(this, NotificationActivity::class.java)
+            val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
             val notification = NotificationCompat.Builder(this, "normal")
                 .setContentTitle("This is content title")
                 .setContentText("This is content text")
                 .setSmallIcon(R.drawable.sync_alt_fill0_wght400_grad0_opsz48)
                 .setLargeIcon(
-                    BitmapFactory.decodeResource(resources, R.drawable.vives_logo))
+                    BitmapFactory.decodeResource(resources, R.drawable.vives_logo)
+                )
+                .setContentIntent(pi)
                 .build()
             manager.notify(1, notification)
         }
---
+++ app/src/main/java/com/example/androidnotifications/NotificationActivity.kt
@@ -0,0 +1,16 @@
+package com.example.androidnotifications
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(1)
+    }
+}
